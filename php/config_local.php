7075307216aabdb215aebf0993beec438d1b2b48
Peter Bond at imac <peter.bond40@gmail.com>
2020-09-09 17:21:03 -0600

    change db connect password and change permissions for all files

--- php/config_local.php
+++ php/config_local.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'bkvit3he55h5hhkf');
+define('DB_PASSWORD', '1212');
+define('DB_DATABASE', 'workglue_demo');
 define('DB_DBMS', 'mysql');
 
 $QB_on = 0;
